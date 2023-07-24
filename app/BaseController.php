3008f37361b01797c8ea8ecd78930210b9780aa6
wagod <18279697517@163.com>
2023-07-24 15:57:02 +0800

    update

--- app/BaseController.php
+++ app/BaseController.php
@@ -1,11 +1,18 @@
 <?php
+/**
+ * @copyright Copyright (c) 2021 勾股工作室
+ * @license https://opensource.org/licenses/Apache-2.0
+ * @link https://www.gougucms.com
+ */
+
 declare (strict_types = 1);
 
 namespace app;
 
 use think\App;
-use think\exception\ValidateException;
-use think\Validate;
+use think\exception\HttpResponseException;
+use think\facade\Request;
+use think\Response;
 
 /**
  * 控制器基础类
@@ -37,58 +44,91 @@ abstract class BaseController
     protected $middleware = [];
 
     /**
+     * 分页数量
+     * @var string
+     */
+    protected $pageSize = '';
+
+    /**
+     * jwt配置
+     * @var string
+     */
+    protected $jwt_conf = [];
+    /**
      * 构造方法
      * @access public
-     * @param  App  $app  应用对象
+     * @param  App $app 应用对象
      */
     public function __construct(App $app)
     {
-        $this->app     = $app;
+        $this->app = $app;
         $this->request = $this->app->request;
-
+        $this->jwt_conf = [
+            'secrect' => env('jwt.secrect'),
+            'iss' => env('jwt.iss'), //签发者 可选
+            'aud' => env('jwt.aud'), //接收该JWT的一方，可选
+            'exptime' => env('jwt.exptime'), //过期时间,这里设置2个小时
+        ];
         // 控制器初始化
         $this->initialize();
     }
 
     // 初始化
     protected function initialize()
-    {}
+    {
+        //每页显示数据量
+        $this->pageSize = Request::param('page_size', \think\facade\Config::get('app.page_size'));
+    }
 
     /**
-     * 验证数据
-     * @access protected
-     * @param  array        $data     数据
-     * @param  string|array $validate 验证器名或者验证规则数组
-     * @param  array        $message  提示信息
-     * @param  bool         $batch    是否批量验证
-     * @return array|string|true
-     * @throws ValidateException
+     * Api处理成功结果返回方法
+     * @param      $message
+     * @param null $redirect
+     * @param null $extra
+     * @return mixed
+     * @throws ReturnException
      */
-    protected function validate(array $data, $validate, array $message = [], bool $batch = false)
+    protected function apiSuccess($msg = 'success',$data=[])
     {
-        if (is_array($validate)) {
-            $v = new Validate();
-            $v->rule($validate);
-        } else {
-            if (strpos($validate, '.')) {
-                // 支持场景
-                [$validate, $scene] = explode('.', $validate);
-            }
-            $class = false !== strpos($validate, '\\') ? $validate : $this->app->parseClass('validate', $validate);
-            $v     = new $class();
-            if (!empty($scene)) {
-                $v->scene($scene);
-            }
-        }
+		return $this->apiReturn($data, 0, $msg);
+    }
 
-        $v->message($message);
+    /**
+     * Api处理结果失败返回方法
+     * @param      $error_code
+     * @param      $message
+     * @param null $redirect
+     * @param null $extra
+     * @return mixed
+     * @throws ReturnException
+     */
+    protected function apiError($msg = 'fail',$data=[], $code = 1)
+    {
+        return $this->apiReturn($data, $code, $msg);
+    }
 
-        // 是否批量验证
-        if ($batch || $this->batchValidate) {
-            $v->batch(true);
-        }
+    /**
+     * 返回封装后的API数据到客户端
+     * @param  mixed   $data 要返回的数据
+     * @param  integer $code 返回的code
+     * @param  mixed   $msg 提示信息
+     * @param  string  $type 返回数据格式
+     * @param  array   $header 发送的Header信息
+     * @return Response
+     */
+    protected function apiReturn($data, int $code = 0, $msg = '', string $type = '', array $header = []): Response
+    {
+        $result = [
+            'code' => $code,
+            'msg' => $msg,
+            'time' => time(),
+            'data' => $data,
+        ];
 
-        return $v->failException(true)->check($data);
+        $type = $type ?: 'json';
+        $response = Response::create($result, $type)->header($header);
+
+        throw new HttpResponseException($response);
     }
 
 }
--- app/common.php
+++ app/common.php
@@ -1,2 +1,116 @@
 <?php
 // 应用公共文件
+
+//获取url参数
+use think\facade\Cache;
+use think\facade\Request;
+
+function get_params($key = "")
+{
+    return Request::instance()->param($key);
+}
+
+//随机字符串，默认长度10
+function set_salt($num = 10)
+{
+    $str = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890';
+    $salt = substr(str_shuffle($str), 10, $num);
+    return $salt;
+}
+
+//密码加密
+function set_password($pwd, $salt)
+{
+    return md5(md5($pwd . $salt) . $salt);
+}
+
+/**
+ * 客户操作日志
+ * @param string $type 操作类型 login reg add edit view delete down join sign play order pay
+ * @param string    $param_str 操作内容
+ * @param int    $param_id 操作内容id
+ * @param array  $param 提交的参数
+ */
+function add_user_log($type, $param_str = '', $param_id = 0, $param = [])
+{
+    $request = request();
+    $title = '未知操作';
+    if(!empty($type)){
+        $title = $type;
+    }
+    if ($type == 'login') {
+        $login_user = \think\facade\Db::name('User')->where(array('id' => $param_id))->find();
+        if ($login_user['nickname'] == '') {
+            $login_user['nickname'] = $login_user['name'];
+        }
+        if ($login_user['nickname'] == '') {
+            $login_user['nickname'] = $login_user['username'];
+        }
+    } else {
+        $login_user = get_login_user();
+        if (empty($login_user)) {
+            $login_user = [];
+            $login_user['id'] = 0;
+            $login_user['nickname'] = '游客';
+        } else {
+            if ($login_user['nickname'] == '') {
+                $login_user['nickname'] = $login_user['username'];
+            }
+        }
+    }
+    $content = $login_user['nickname'] . '在' . date('Y-m-d H:i:s') . '执行了' . $title . '操作';
+    if ($param_str != '') {
+        $content = $login_user['nickname'] . '在' . date('Y-m-d H:i:s') . $title . '了' . $param_str;
+    }
+    $data = [];
+    $data['uid'] = $login_user['id'];
+    $data['nickname'] = $login_user['nickname'];
+    $data['type'] = $type;
+    $data['title'] = $title;
+    $data['content'] = $content;
+    $data['param_id'] = $param_id;
+    $data['param'] = json_encode($param);
+    $data['module'] = strtolower(app('http')->getName());
+    $data['controller'] = strtolower(app('request')->controller());
+    $data['function'] = strtolower(app('request')->action());
+    $data['ip'] = app('request')->ip();
+    $data['create_time'] = time();
+    \think\facade\Db::name('UserLog')->strict(false)->field(true)->insert($data);
+}
+
+function get_login_user($key = "")
+{
+    $session_user = get_cache('app.session_user');
+    if (\think\facade\Session::has($session_user)) {
+        $gougu_user = \think\facade\Session::get($session_user);
+        if (!empty($key)) {
+            if (isset($gougu_user[$key])) {
+                return $gougu_user[$key];
+            } else {
+                return '';
+            }
+        } else {
+            return $gougu_user;
+        }
+    } else {
+        return '';
+    }
+}
+
+//设置缓存
+function set_cache($key, $value, $date = 86400)
+{
+    Cache::set($key, $value, $date);
+}
+
+//读取缓存
+function get_cache($key)
+{
+    return Cache::get($key);
+}
+
+//清空缓存
+function clear_cache($key)
+{
+    Cache::clear($key);
+}
