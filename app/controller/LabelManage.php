68982e446c5fddb3dc61be67eae99f0695092af1
wagod <18279697517@163.com>
2023-07-25 10:50:51 +0800

    update

---
+++ app/controller/LabelManage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controller;
+
+use app\BaseController;
+
+class LabelManage extends BaseController
+{
+    public function index(){
+        $params = get_params();
+
+    }
+}
