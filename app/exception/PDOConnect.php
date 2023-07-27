716ca67c8a1df1289f84dc12da5d4c8e98c77cfa
xiaowenfu <88598401@qq.com>
2023-07-27 12:02:50 +0800

    封装PDO的CRUD

--- app/exception/PDOConnect.php
+++ app/exception/PDOConnect.php
@@ -1,11 +1,74 @@
 <?php
 
 namespace app\exception;
-
 class PDOConnect
 {
-    public function __construct($config)
-    {
 
+    private $pdo;
+
+    public function __construct($config=[])
+    {
+        // 尝试连接到数据库
+        try {
+            $this->pdo = new PDO(
+                'mysql:host=' . $config['host'] . ';dbname=' . $config['db'],
+                $config['username'],
+                $config['password']
+            );
+            // 设置PDO错误模式为异常
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            // 连接失败，输出错误信息
+            die('连接数据库失败: ' . $e->getMessage());
+        }
+    }
+
+    // 查询操作
+    public function query($sql, $params = [])
+    {
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die('查询失败: ' . $e->getMessage());
+        }
+    }
+
+    // 插入操作
+    public function insert($sql, $params = [])
+    {
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $this->pdo->lastInsertId();
+        } catch (PDOException $e) {
+            die('插入失败: ' . $e->getMessage());
+        }
+    }
+
+    // 更新操作
+    public function update($sql, $params = [])
+    {
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            die('更新失败: ' . $e->getMessage());
+        }
+    }
+
+    // 删除操作
+    public function delete($sql, $params = [])
+    {
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($params);
+            return $stmt->rowCount();
+        } catch (PDOException $e) {
+            die('删除失败: ' . $e->getMessage());
+        }
     }
 }
+
