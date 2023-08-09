99b28cca0d4d1cd1aeac5c73886c865f77cd2408
xiaowenfu <88598401@qq.com>
2023-08-09 11:19:58 +0800

    excel上传gpt的key

--- app/controller/Chatkey.php
+++ app/controller/Chatkey.php
@@ -10,7 +10,9 @@ namespace app\controller;
 use app\model\ChatKeyModel;
 use app\BaseController;
 use think\facade\Db;
-
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class chatkey extends BaseController
 {
@@ -79,5 +81,74 @@ class chatkey extends BaseController
                 $this->apiSuccess("删除失败");
             }
         }
+    }
+
+
+    function batchupload(){
+        $file = request()->file('excel');
+        if(request()->file('excel')){
+            $file = request()->file('excel');
+        } else{
+            $this->apiError('没有选择上传文件');
+        }
+        $md5=md5(time());
+        //echo $md5.'----------------------';
+        $dataPath = date('Ymd');
+        $filename = \think\facade\Filesystem::disk('public')->putFile($dataPath, $file, function () use ($md5) {
+            return $md5;
+        });
+        $info = explode('.', $filename);
+        $file_extension = $info[1];
+        if ($file_extension == 'xlsx') {
+            $objReader = IOFactory::createReader('Xlsx');
+        } else {
+            $objReader = IOFactory::createReader('Xls');
+        }
+
+        $rule = [
+            'image' => 'jpg,png,jpeg,gif,ico',
+            'doc' => 'xls,xlsx',
+            'file' => 'zip',
+            'video' => 'mpg,mp4,mpeg,avi,wmv,mov,flv,m4v',
+            'ico'   =>'ico',
+        ];
+        $validate = \think\facade\Validate::rule([
+            'image' => 'require|fileExt:' . $rule['doc'],
+        ]);
+        $file_check['image'] = $file;
+        if (!$validate->check($file_check)) {
+            $this->apiError($validate->getError());
+        }
+        $filePath =  $_SERVER['DOCUMENT_ROOT'].'/storage/'.$filename;
+        $phpexcel = $objReader->load($filePath);
+        $sheet = $phpexcel->getSheet(0);
+        $highestRow = $sheet->getHighestRow();
+        $highestColumn = $sheet->getHighestColumn();
+        $data = [];
+        $rows = [];
+        for($row=2;$row<=$highestRow;$row++){
+            $rowData = [];
+            for($col='A';$col<=$highestColumn;$col++){
+                $rowData[$col] = (string)$sheet->getCell($col.$row);
+                $value = (string)$sheet->getCell($col.$row);
+                if($col=='A' && !empty($value) && !in_array($value,$data)){
+                    $data[]=$value;
+                }
+            }
+            $rows[]=$rowData;
+        }
+        $model = new ChatKeyModel();
+        $insert = [];
+        foreach ($rows as $row){
+            if(!$model->where("key",$row['A'])->find()){
+                $insert[]=[
+                    'key' => $row['A'],
+                    'createdAt' => time(),
+                    'updateAt' => time(),
+                ];
+            }
+        }
+        ChatKeyModel::insertAll($insert);
+        $this->apiSuccess("处理成功");
     }
 }
